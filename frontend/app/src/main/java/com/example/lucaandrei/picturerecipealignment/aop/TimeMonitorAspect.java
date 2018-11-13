e6286efa7e29b37e923b4806bba967c5fad4efc8
DESKTOP-L8H23GJ\Luca Andrei <luca.andrei96@gmail.com>
2018-11-13 21:57:26 +0200

    Added TimeMonitorAspect

---
+++ frontend/app/src/main/java/com/example/lucaandrei/picturerecipealignment/aop/TimeMonitorAspect.java
@@ -0,0 +1,28 @@
+package com.example.lucaandrei.picturerecipealignment.aop;
+
+import android.util.Log;
+import android.view.View;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class TimeMonitorAspect {
+    public long startedTime = 0;
+    public static String TAG = "Time-ASPECT";
+
+    @Pointcut("execution(void *.onButton*Click(..))")
+    public void onButtonClick() {}
+
+    @Before("onButtonClick() && args(view)")
+    public void beforeButton(View view) {
+        startedTime = System.currentTimeMillis();
+    }
+
+    @After("onButtonClick() && args(view)")
+    public void afterButton(View view) {
+        Log.i(TAG, String.format("Execution took <%s> milliseconds.", System.currentTimeMillis() - startedTime));
+    }
+}
