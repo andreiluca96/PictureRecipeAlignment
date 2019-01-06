9e17ab25b286b25bd5371d9ef96d9db712305177
Ioana <ioasitaru@yahoo.com>
2019-01-06 15:57:12 +0200

    Added missing scripts

---
+++ model/util/truncate_det_ingr.py
@@ -0,0 +1,19 @@
+import json
+
+NO_ITEMS = 2
+DET_INGRS_PATH = "../../../data/det_ingrs.json"
+
+if __name__ == "__main__":
+    with open("truncated-det-ingrs-" + str(NO_ITEMS) + ".json", "w") as truncated_det_ingrs_file:
+        with open("truncated-layer2-" + str(NO_ITEMS) + ".json") as truncated_layer2_file:
+            with open(DET_INGRS_PATH) as det_ingrs_file:
+                layer2_data = json.load(truncated_layer2_file)
+                det_ingrs_data = json.load(det_ingrs_file)
+
+                truncated_data = []
+                for layer2_item in layer2_data:
+                    matching_items = [x for x in det_ingrs_data if x['id'] == layer2_item['id']]
+                    if len(matching_items) > 0:
+                        truncated_data.append(matching_items[0])
+
+                json.dump(truncated_data, truncated_det_ingrs_file, indent=4)
